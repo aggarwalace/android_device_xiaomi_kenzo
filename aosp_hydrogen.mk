d2808cdf7e3be5baa7e2be7db3bdddf5d4e425a1
Ace Aggarwal <aggarwalace@gmail.com>
2017-09-17 22:03:44 -0700

    Adapt for hydrogen

--- aosp_hydrogen.mk
+++ aosp_hydrogen.mk
@@ -18,20 +18,20 @@
 # $(call inherit-product, device/sample/products/backup_overlay.mk)
 
 # Provide meaningful APN configuration
-PRODUCT_COPY_FILES := device/xiaomi/kenzo/configs/apns-full-conf.xml:system/etc/apns-conf.xml
+PRODUCT_COPY_FILES := device/xiaomi/hydrogen/configs/apns-full-conf.xml:system/etc/apns-conf.xml
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-PRODUCT_NAME := aosp_kenzo
-PRODUCT_DEVICE := kenzo
+PRODUCT_NAME := aosp_hydrogen
+PRODUCT_DEVICE := hydrogen
 PRODUCT_BRAND := Xiaomi
-PRODUCT_MODEL := Redmi Note 3
+PRODUCT_MODEL := Mi Max
 PRODUCT_MANUFACTURER := xiaomi
 
-$(call inherit-product, device/xiaomi/kenzo/device.mk)
-$(call inherit-product-if-exists, vendor/xiaomi/kenzo/device-vendor.mk)
+$(call inherit-product, device/xiaomi/hydrogen/device.mk)
+$(call inherit-product-if-exists, vendor/xiaomi/hydrogen/device-vendor.mk)
 
 PRODUCT_PACKAGES += \
     Launcher3
